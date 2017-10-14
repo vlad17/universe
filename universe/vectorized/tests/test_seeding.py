1bce620f91a0aba913e00afdb41cfb97c1259b26
Vladimir Feinberg <vladf@berkeley.edu>
2017-10-14 16:25:52 -0700

    added broken seed test

---
+++ universe/vectorized/tests/test_seeding.py
@@ -0,0 +1,42 @@
+import unittest
+
+from gym import Env
+from gym.envs import register
+from universe.vectorized import MultiprocessingEnv
+
+
+class SeedEnv(Env):
+    def __init__(self):
+        self._seed_value = None
+
+    def _seed(self, seed=None):
+        self._seed_value = seed
+        return [seed]
+
+    def _reset(self):
+        return self._seed_value
+
+
+class TestMultiProcessingSeedEnv(unittest.TestCase):
+    def __init__(self):
+        super().__init__()
+        self.env_id = None
+
+    def setUp(self):
+        super().setUp()
+        env = SeedEnv()
+        self.env_id = env.__class__.__name__ + '-v0'
+        entry = env.__class__.__module__ + ':' + env.__class__.__name__
+        register(self.env_id, entry_point=entry)
+
+    def test_multiprocessing_env_seed(self):
+        venv = MultiprocessingEnv(self.env_id)
+        venv.configure(n=2)
+        venv.seed([1, 2])
+        seed1, seed2 = venv.reset()
+        self.assertEqual(seed1, 1)
+        self.assertEqual(seed2, 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
